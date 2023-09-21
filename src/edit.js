192063438f2fb359ddd1de0c66cf5c917daa9dd0
Dan Stoakes <dan.stoakes8@gmail.com>
2023-09-21 20:42:12 +0100

    Add index.js and edit.js

---
+++ src/edit.js
@@ -0,0 +1,126 @@
+import { __ } from '@wordpress/i18n';
+
+import {
+    MediaUpload,
+    MediaUploadCheck,
+    RichText,
+    InspectorControls,
+    BlockControls,
+    AlignmentToolbar,
+} from '@wordpress/block-editor';
+
+import {
+    PanelBody,
+    PanelRow,
+    TextControl,
+    Button,
+} from '@wordpress/components';
+
+export default function Edit ({ attributes, setAttributes, isSelected }) {
+    const { title, content, mediaID, mediaURL, alignment } = attributes;
+
+    const onChangeTitle = (newTitle) => {
+        setAttributes({ title: newTitle });
+    };
+
+    const onChangeContent = (newContent) => {
+        setAttributes({ content: newContent });
+    };
+
+    const onSelectImage = (media) => {
+        setAttributes({
+            mediaID: media.id,
+            mediaURL: media.url,
+        });
+    };
+
+    const onImageRemove = () => {
+        setAttributes({
+            mediaID: null,
+            mediaURL: '',
+        });
+    };
+
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody title={__('Block Settings')}>
+                    <TextControl
+                        label={__('Title')}
+                        value={title}
+                        onChange={onChangeTitle}
+                    />
+                    <PanelRow>
+                        <strong>{__('Image Alignment')}</strong>
+                    </PanelRow>
+                    <PanelRow>
+                        <AlignmentToolbar
+                            value={alignment}
+                            onChange={(value) => setAttributes({ alignment: value })}
+                        />
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+            <BlockControls>
+                <AlignmentToolbar
+                    value={alignment}
+                    onChange={(value) => setAttributes({ alignment: value })}
+                />
+            </BlockControls>
+            <div className={`align${alignment}`}>
+                <div className="image">
+                    <MediaUploadCheck>
+                        <MediaUpload
+                            onSelect={onSelectImage}
+                            allowedTypes={['image']}
+                            value={mediaID}
+                            render={({ open }) => (
+                                <div>
+                                    {mediaID ? (
+                                        <>
+                                            <img
+                                                src={mediaURL}
+                                                alt=""
+                                                style={{
+                                                    maxWidth: '100%',
+                                                }}
+                                            />
+                                            {isSelected && (
+                                                <Button
+                                                    isLink
+                                                    isDestructive
+                                                    onClick={onImageRemove}
+                                                >
+                                                    {__('Delete Image', 'danstoakes-img-content-two-col')}
+                                                </Button>
+                                            )}
+                                        </>
+                                    ) : (
+                                        <Button onClick={open}>
+                                            {__('Select Image', 'danstoakes-img-content-two-col')}
+                                        </Button>
+                                    )}
+                                </div>
+                            )}
+                        />
+                    </MediaUploadCheck>
+                </div>
+                <div className="content">
+                    <RichText
+                        tagName="h2"
+                        placeholder={__('Enter title...', 'danstoakes-img-content-two-col')}
+                        value={title}
+                        onChange={onChangeTitle}
+                    />
+                    <RichText
+                        tagName="div"
+                        placeholder={__('Enter content...', 'danstoakes-img-content-two-col')}
+                        multiline="p"
+                        value={content}
+                        onChange={onChangeContent}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { registerBlockType } from '@wordpress/blocks';
+
+import './style.scss';
+
+import edit from './edit';
+import save from './save';
+
+import metadata from './block.json';
+
+registerBlockType(metadata.name, {
+    edit,
+    save,
+});
